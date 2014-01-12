46910f9685b01ce42759eff52274a2a3731e2ca1
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2014-01-12 11:53:51 +0000

    Add cut action

--- pipe-gui/src/main/java/pipe/actions/edit/CutAction.java
+++ pipe-gui/src/main/java/pipe/actions/edit/CutAction.java
@@ -1,18 +1,24 @@
 package pipe.actions.edit;
 
 import pipe.actions.GuiAction;
+import pipe.controllers.PetriNetController;
+import pipe.controllers.PipeApplicationController;
 
 import java.awt.event.ActionEvent;
 
 public class CutAction extends GuiAction {
+    private final PipeApplicationController applicationController;
 
     public CutAction(final String name, final String tooltip,
-                     final String keystroke) {
+                     final String keystroke, PipeApplicationController applicationController) {
         super(name, tooltip, keystroke);
+        this.applicationController = applicationController;
     }
 
     @Override
     public void actionPerformed(final ActionEvent actionEvent) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        PetriNetController petriNetController = applicationController.getActivePetriNetController();
+        petriNetController.copySelection();
+        petriNetController.deleteSelection();
     }
 }
