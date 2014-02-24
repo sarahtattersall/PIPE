51ffba518f545d0c6695772273c3fd0052966eb8
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2014-02-24 18:17:18 +0000

    Remove all IDE files
    * IDE files are not integral to project and can be generated with
      Eclipse

--- launch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd Pipe
-java -classpath .:./lib/jpowergraph-0.2-common.jar:./lib/jpowergraph-0.2-swing.jar:./lib/powerswing-0.3.jar:./lib/drmaa.jar:./lib/hadoop-0.13.1-dev-core.jar:./lib/jcommon-1.0.10.jar:./lib/jfreechart-1.0.6.jar:./lib/jfreechart-1.0.6-swt.jar:./lib/tools.jar:.lib/jeval.jar Pipe
