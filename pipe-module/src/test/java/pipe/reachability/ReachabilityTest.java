fabafdbb1d7854e07551698c6805fd95169f6181
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2014-04-26 18:15:09 +0100

    Add test for vanishing cyclic states
    * Unlike PIPE 4, we must sum up all the state transitions after the
      reachability file has been generated.
    * CHECK WITH WILL but I think this is better, the previous
      implementation stores the transitions to write in memory - which then
      makes all the memory saving tricks in his 1996 PhD pointless since
      we're storing all the transitions anyway!

--- pipe-module/src/test/java/pipe/reachability/ReachabilityTest.java
+++ pipe-module/src/test/java/pipe/reachability/ReachabilityTest.java
@@ -2,6 +2,7 @@ package pipe.reachability;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,6 +24,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ReachabilityTest {
@@ -33,6 +35,11 @@ public class ReachabilityTest {
     public void setUp() {
         formatter = new ByteWriterFormatter();
         outputStream = new ByteArrayOutputStream();
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        outputStream.close();
     }
 
     @Test
@@ -66,14 +73,31 @@ public class ReachabilityTest {
                 createRecord("{\"3\": 0, \"2\": 0, \"1\": 1, \"7\": 0, \"6\": 0, \"5\": 0, \"4\": 0, \"8\": 0}", "{\"3\": 0, \"2\": 0, \"1\": 0, \"7\": 0, \"6\": 0, \"5\": 0, \"4\": 0, \"8\": 1}", 0.50));
     }
 
+    @Test
+    public void cyclicVanishingState()
+            throws JAXBException, UnparsableException, PetriNetComponentNotFoundException, IOException {
+        PetriNet petriNet = Utils.readPetriNet("/cyclic_vanishing.xml");
+        Token token = petriNet.getComponent("Default", Token.class);
+        Reachability reachability = new Reachability(petriNet, token, formatter);
+        reachability.generate(outputStream);
+        verifyOutput(outputToInputStream(outputStream),
+                createRecord("{\"1\": 1, \"2\": 0, \"3\": 0, \"4\": 0, \"5\": 0, \"6\": 0, \"7\": 0, \"8\": 0}", "{\"1\": 0, \"2\": 0, \"3\": 0, \"4\": 0, \"5\": 0, \"6\": 0, \"7\": 1, \"8\": 0}", 2.325),
+                createRecord("{\"1\": 1, \"2\": 0, \"3\": 0, \"4\": 0, \"5\": 0, \"6\": 0, \"7\": 0, \"8\": 0}", "{\"1\": 0, \"2\": 0, \"3\": 0, \"4\": 0, \"5\": 0, \"6\": 0, \"7\": 0, \"8\": 1}", 3.875),
+                createRecord("{\"1\": 1, \"2\": 0, \"3\": 0, \"4\": 0, \"5\": 0, \"6\": 0, \"7\": 0, \"8\": 0}", "{\"1\": 0, \"2\": 0, \"3\": 0, \"4\": 0, \"5\": 0, \"6\": 1, \"7\": 0, \"8\": 0}", 1.8));
+    }
+
     public InputStream outputToInputStream(ByteArrayOutputStream stream) {
         return new ByteArrayInputStream(stream.toByteArray());
     }
 
     public void verifyOutput(InputStream inputStream, Record... records) throws IOException {
+
+        Map<StateTransition, Double> actualRecords = getTotalRates(inputStream);
+        assertEquals(records.length, actualRecords.size());
         for (Record expected : records) {
-            Record actual = formatter.read(inputStream);
-            assertEquals(expected, actual);
+            Double rate = actualRecords.get(new StateTransition(expected.state, expected.successor));
+            assertNotNull(rate);
+            assertEquals(expected.rate, rate, 0.0001);
         }
     }
 
@@ -89,4 +113,58 @@ public class ReachabilityTest {
         return new Record(state, successor, rate);
     }
 
+    private Map<StateTransition, Double> getTotalRates(InputStream inputStream) throws IOException {
+        Map<StateTransition, Double> result = new HashMap<>();
+        while(inputStream.available() > 0) {
+            Record record = formatter.read(inputStream);
+            StateTransition transition = new StateTransition(record.state, record.successor);
+
+            if (!result.containsKey(transition)) {
+                result.put(transition, .0);
+            }
+            result.put(transition, result.get(transition) + record.rate);
+        }
+        inputStream.close();
+        return result;
+    }
+
+    private class StateTransition {
+        private final State state;
+
+        private StateTransition(State state, State successor) {
+            this.state = state;
+            this.successor = successor;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof StateTransition)) {
+                return false;
+            }
+
+            StateTransition that = (StateTransition) o;
+
+            if (!state.equals(that.state)) {
+                return false;
+            }
+            if (!successor.equals(that.successor)) {
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = state.hashCode();
+            result = 31 * result + successor.hashCode();
+            return result;
+        }
+
+        private final State successor;
+
+    }
 }
