d2669b8ca51131ffd493730d17c922646eb1dc11
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2014-05-06 18:07:09 +0100

    Create AbstractStateSpaceExplorer #54
    * Implementations of StateSpaceExplorer can now extend this
    * Contains all shared code
    * Subclasses just implement the stateSpaceExploration method since this
      is all that differs

---
+++ pipe-module/src/main/java/pipe/reachability/algorithm/parallel/IndividualParallelStateSpaceExplorer.java
@@ -0,0 +1,78 @@
+package pipe.reachability.algorithm.parallel;
+
+import pipe.reachability.algorithm.AbstractStateSpaceExplorer;
+import pipe.reachability.algorithm.ExplorerUtilities;
+import pipe.reachability.algorithm.TimelessTrapException;
+import pipe.reachability.algorithm.VanishingExplorer;
+import pipe.reachability.algorithm.state.StateWriter;
+import pipe.reachability.state.ExplorerState;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.*;
+
+/**
+ * This explores individual states on seperate threads and then joins
+ * their results together on the master thread.
+ *
+ * Yields speed ups for larger state spaces but is marginally slower for smaller ones
+ */
+public class IndividualParallelStateSpaceExplorer extends AbstractStateSpaceExplorer {
+
+    public IndividualParallelStateSpaceExplorer(StateWriter stateWriter, VanishingExplorer vanishingExplorer,
+                                                ExplorerUtilities explorerUtilities) {
+        super(explorerUtilities, vanishingExplorer, stateWriter);
+        executorService = Executors.newFixedThreadPool(8);
+
+    }
+    protected ExecutorService executorService;
+
+    @Override
+    protected void stateSpaceExploration() throws InterruptedException, ExecutionException, TimelessTrapException {
+        if (executorService.isTerminated()) {
+            executorService = Executors.newFixedThreadPool(8);
+        }
+        int elemsAtCurrentLevel = explorationQueue.size();
+        int elemsAtNextLevel = 0;
+        while (!explorationQueue.isEmpty()) {
+
+            Map<ExplorerState, Future<Map<ExplorerState, Double>>> successorFutures = new HashMap<>();
+            CountDownLatch latch = new CountDownLatch(elemsAtCurrentLevel);
+            for (int i = 0; i < elemsAtCurrentLevel; i++) {
+                ExplorerState state = explorationQueue.poll();
+                successorFutures.put(state, executorService.submit(
+                        new ParallelStateExplorer(latch, state, explorerUtilities, vanishingExplorer)));
+            }
+
+            latch.await();
+            for (Map.Entry<ExplorerState, Future<Map<ExplorerState, Double>>> entry : successorFutures.entrySet()) {
+                Future<Map<ExplorerState, Double>> future = entry.getValue();
+                ExplorerState state = entry.getKey();
+                successorRates.clear();
+
+                try {
+                    Map<ExplorerState, Double> successors = future.get();
+                    for (Map.Entry<ExplorerState, Double> successorEntry : successors.entrySet()) {
+                        ExplorerState successor = successorEntry.getKey();
+                        double rate = successorEntry.getValue();
+                        registerStateRate(successor, rate);
+                        if (!explored.contains(successor)) {
+                            elemsAtNextLevel++;
+                            explorationQueue.add(successor);
+                            markAsExplored(successor);
+                        }
+                    }
+                } catch (ExecutionException ee) {
+                    throw new TimelessTrapException();
+                }
+                writeStateTransitions(state);
+            }
+            elemsAtCurrentLevel = elemsAtNextLevel;
+            elemsAtNextLevel = 0;
+
+        }
+        executorService.shutdownNow();
+
+    }
+
+}
