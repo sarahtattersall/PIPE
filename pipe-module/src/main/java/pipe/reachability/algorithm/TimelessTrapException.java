795c901b420ad24ae3ba4b20d88e5dc4efb78718
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2014-05-12 14:30:02 +0100

    Remove pipe-module and move to its own PipeAnalysis repo
    * Update poms accordingly

--- pipe-module/src/main/java/pipe/reachability/algorithm/TimelessTrapException.java
+++
@@ -1,8 +0,0 @@
-package pipe.reachability.algorithm;
-
-/**
- * Timeless trap exception is thrown when state space exploration cannot
- * get out of a cyclic vanishing state.
- */
-public class TimelessTrapException extends Exception {
-}
