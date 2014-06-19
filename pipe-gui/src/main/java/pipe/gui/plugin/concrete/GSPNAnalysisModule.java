597d3a49591e24179eec7c3d17e30d459b66f218
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2014-06-19 10:37:25 +0100

    Add GSPN analysis to PIPE

---
+++ pipe-gui/src/main/java/pipe/gui/plugin/concrete/GSPNAnalysisModule.java
@@ -0,0 +1,34 @@
+package pipe.gui.plugin.concrete;
+
+import pipe.gui.analysis.GSPNAnalysis;
+import pipe.gui.plugin.GuiModule;
+import uk.ac.imperial.pipe.models.petrinet.PetriNet;
+
+import javax.swing.*;
+import java.awt.FileDialog;
+
+public class GSPNAnalysisModule implements GuiModule {
+    /**
+     * Starts the GSPN analysis module
+     * @param petriNet current Petri net to use
+     */
+    @Override
+    public void start(PetriNet petriNet) {
+        JFrame frame = new JFrame("GSPN analysis");
+        FileDialog selector = new FileDialog(frame, "Select petri net", FileDialog.LOAD);
+
+        frame.setContentPane(new GSPNAnalysis(petriNet, selector).getMainPanel());
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    /**
+     *
+     * @return GSPN Analysis
+     */
+    @Override
+    public String getName() {
+        return "GSPN Analysis";
+    }
+}
