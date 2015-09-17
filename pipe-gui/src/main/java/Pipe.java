0cab1f8376f97c967f09d6cd2f73eb0c2f7c43ef
Steve Doubleday <stevedoubleday@gmail.com>
2015-09-16 17:25:23 -0700

    test that PlaceView is notified of token changes

--- pipe-gui/src/main/java/Pipe.java
+++ pipe-gui/src/main/java/Pipe.java
@@ -28,7 +28,7 @@ public final class Pipe {
         return new Runnable() {
             @Override
             public void run() {
-                new Pipe("v5.0.0");
+                new Pipe("v5.1.0");
             }
         };
     }
