8d653ee5419849fae1e8b6255043ee8e0d8b4bc1
Wladislaw Mitzel <mitzel@tawadi.de>
2015-12-03 22:04:34 +0100

    Fix NPE in PipeApplicationView. Use slash as path separator for loading of resources.

--- pipe-gui/src/main/java/pipe/gui/PIPEConstants.java
+++ pipe-gui/src/main/java/pipe/gui/PIPEConstants.java
@@ -9,12 +9,12 @@ public final class PIPEConstants {
     /**
      * Resources directory of images
      */
-    public static final String IMAGE_PATH = File.separator + "images" + File.separator;
+    public static final String IMAGE_PATH = "/images" + File.separator;
 
     /**
      * Path to the directory that the PIPE examples are located in
      */
-    public static final String EXAMPLES_PATH = File.separator + "extras" + File.separator + "examples" + File.separator;
+    public static final String EXAMPLES_PATH = "/extras" + File.separator + "examples" + File.separator;
 
     /**
      * Hidden constructor for utility class
