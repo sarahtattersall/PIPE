f4ad8f170d110803726e83bfdb97a140d313eb3d
Steve Doubleday <stevedoubleday@gmail.com>
2016-08-04 22:05:14 -0700

    type conversion changes for java 1.8 compatibility

--- pipe-gui/src/test/java/pipe/actions/type/TimedTransactionActionTest.java
+++ pipe-gui/src/test/java/pipe/actions/type/TimedTransactionActionTest.java
@@ -1,30 +1,35 @@
 package pipe.actions.type;
 
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+
+import javax.swing.event.UndoableEditListener;
+import javax.swing.undo.UndoableEdit;
+
 import matchers.component.HasMultiple;
 import matchers.component.HasTimed;
 import matchers.component.HasXY;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import pipe.actions.gui.PipeApplicationModel;
 import pipe.actions.gui.TimedTransitionAction;
 import pipe.actions.gui.TransitionAction;
 import pipe.controllers.PetriNetController;
-import pipe.actions.gui.PipeApplicationModel;
 import pipe.historyActions.component.AddPetriNetObject;
 import pipe.utilities.transformers.Contains;
 import uk.ac.imperial.pipe.models.petrinet.DiscreteTransition;
-import uk.ac.imperial.pipe.models.petrinet.Transition;
 import uk.ac.imperial.pipe.models.petrinet.PetriNet;
-
-import javax.swing.event.UndoableEditListener;
-import javax.swing.undo.UndoableEdit;
-import java.awt.Point;
-import java.awt.event.MouseEvent;
-
-import static org.mockito.Matchers.argThat;
-import static org.mockito.Mockito.*;
+import uk.ac.imperial.pipe.models.petrinet.Transition;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TimedTransactionActionTest {
@@ -53,7 +58,8 @@ public class TimedTransactionActionTest {
     }
 
 
-    @Test
+    @SuppressWarnings("unchecked")
+	@Test
     public void createsTimedTransitionOnClick() {
         Point point = new Point(10, 20);
         MouseEvent mockEvent = mock(MouseEvent.class);
@@ -61,9 +67,8 @@ public class TimedTransactionActionTest {
         when(mockEvent.getPoint()).thenReturn(point);
 
         action.doAction(mockEvent, mockController);
-
         verify(mockNet).addTransition(
-                argThat(new HasMultiple<>(new HasXY(point.getX(), point.getY()), new HasTimed(true))));
+                argThat(new HasMultiple<>(new HasXY<Transition>(point.getX(), point.getY()), new HasTimed(true))));
     }
 
     @Test
